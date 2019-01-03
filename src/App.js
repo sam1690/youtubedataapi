1b9c81333fc9a622ba4a2426d9d16a088e6ac06e
sam1690 <engg.smishra@gmail.com>
2019-01-03 09:48:38 +0530

    major branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      apidata : "data from youtube......"
+      apidata : "data from youtube........."
     }
   }
   onsearch=()=>{
