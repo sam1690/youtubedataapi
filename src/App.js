c959039bf31cda245990378eb9a43987b6ccf1d6
sam1690 <engg.smishra@gmail.com>
2019-01-03 09:18:55 +0530

    final

--- src/App.js
+++ src/App.js
@@ -6,28 +6,53 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      apidata : []
+      videodisplay:{videoid:"", title:""},
+      thumbnailtitle:[]
     }
   }
   onsearch=()=>{
     fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAvaEX0RtArjGSkbJ7opxRTWeagwC9dEHQ&maxResults=5&type=video&q="+this.refs.searchtext.value)
     .then((res)=>res.json())
     .then((res)=>{
-      this.setState({apidata:res.items.map((key)=>"https://www.youtube.com/embed/"+key.id.videoId)})
-      console.log(this.state)
+      
+      this.setState({thumbnailtitle:res.items.map(function(elem) {
+        return {
+          url: elem.snippet.thumbnails.default.url,
+          title: elem.snippet.title,
+          videoid:"https://www.youtube.com/embed/"+elem.id.videoId
+        } 
+      })})
+      console.log(this.state.thumbnailtitle)
+      //console.log(res)
     })
     //console.log(this.refs.searchtext.value)
   }
+
+  videodis=(videoid, title)=>{
+    //console.log(videoid)
+    this.setState({videodisplay:{videoid:videoid,title:title}})
+    console.log(this.state.videodisplay)
+
+    
+  }
   render() {
+    
     return (
       <div className="App">
       <div>
-       <input ref="searchtext" type="text" placeholder="enter keyword"/>
+        <h3 style={{marginRight:"50px"}}>YOUTUBEE</h3>
+       <input ref="searchtext" type="text" placeholder="enter keyword" style={{margin:"10px"}}/>
        <button onClick={()=>this.onsearch()}>SEARCH</button>
-       </div>       
-      {this.state.apidata.map((key, id)=><iframe width="400" height="315" key={id} src={key} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>)}
+      </div> 
+      <hr></hr>
+      <div style={{float:"right"}}>
+        {this.state.thumbnailtitle.map((item, index)=><div key={index} onClick={()=>this.videodis(item.videoid,item.title)}><img src={item.url}/><p>{item.title}</p><p>{item.videoid}</p><hr></hr></div>)}      
+       </div> 
+       <h2>{this.state.videodisplay.title}</h2>
+        <iframe width="560" height="315" src={this.state.videodisplay.videoid} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
     );
+    
   }
 }
 
